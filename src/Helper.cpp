fda52c5e41127903ad64c1c7b6cf8ebdc4facf97
amirphl <amirphl4@gmail.com>
2025-02-26 14:08:04 +0000

    Initial commit

---
+++ src/Helper.cpp
@@ -0,0 +1,15 @@
+#include <cmath>
+#include <cstdlib>
+#include <fstream>
+#include <iomanip>
+#include <iostream>
+#include <memory>
+#include <nlohmann/json.hpp>
+#include <random>
+#include <sstream>
+#include <string>
+#include <unordered_map>
+#include <uuid/uuid.h>
+#include <vector>
+
+using json = nlohmann::json;
