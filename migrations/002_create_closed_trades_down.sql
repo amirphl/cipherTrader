6f974fd5730f5dbd8bf92d95e362afd652a715b8
Amir <amirphl4@gmail.com>
2025-04-06 15:50:59 +0330

    feat: Add cleanup script for closed_trades and daily_balances tables
    - Drop daily_balances related objects:
      - Index: idx_daily_balances_unique
      - Index: idx_daily_balances_lookup
      - Table: daily_balances
    
    - Drop closed_trades related objects:
      - Index: idx_closed_trades_filter
      - Table: closed_trades
    
    - Key features:
      - Safe cleanup with IF EXISTS clauses
      - Complete removal of schema objects

---
+++ migrations/002_create_closed_trades_down.sql
@@ -0,0 +1,2 @@
+DROP INDEX IF EXISTS idx_closed_trades_filter;
+DROP TABLE IF EXISTS closed_trades;
---
+++ migrations/003_create_daily_balances_down.sql
@@ -0,0 +1,3 @@
+DROP INDEX IF EXISTS idx_daily_balances_unique;
+DROP INDEX IF EXISTS idx_daily_balances_lookup;
+DROP TABLE IF EXISTS daily_balances;
