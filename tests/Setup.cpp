4d4eae000cdbe9a0957abc08cc25a24e8f1b1acc
Amir <amirphl4@gmail.com>
2025-04-22 01:57:41 +0330

    Add global test environment with enhanced logging
    - Introduce GlobalEnvironment class for test suite setup/teardown
    - Replace std::cout with CipherLog::LOG.info for global setup/teardown messages
    - Initialize CipherLog::Logger with Debug level and console/stderr output
    - Disable file output for tests, specify log file path as logs/cipher_trader_tests.log
    - Register GlobalEnvironment with Google Test framework

---
+++ tests/Setup.cpp
@@ -0,0 +1,30 @@
+// In one of your test files (or a separate setup.cpp)
+#include "Logger.hpp"
+#include <gtest/gtest.h>
+
+class GlobalEnvironment : public ::testing::Environment
+{
+   public:
+    void SetUp() override
+    {
+        // Global initialization here
+        std::cout << "Global setup for all tests" << std::endl;
+
+        CipherLog::Logger::getInstance().init("CipherTrader",
+                                              CipherLog::LogLevel::Debug, // Set global level to Debug
+                                              true,                       // Enable console output
+                                              true,                       // Enable stderr output
+                                              false,                      // Disable file output
+                                              "logs/cipher_trader_tests.log");
+    }
+
+    void TearDown() override
+    {
+        // Global cleanup here
+        std::cout << "Global cleanup after all tests" << std::endl;
+    }
+};
+
+// Register the environment - should be in the same file
+// This needs to be outside of any namespace
+::testing::Environment* const env = ::testing::AddGlobalTestEnvironment(new GlobalEnvironment());
